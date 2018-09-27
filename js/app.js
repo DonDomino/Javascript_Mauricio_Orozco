48fb40a8e6360f1df6a7c560548de61a4361c3c1
Mauricio <maudob@live.com>
2018-09-26 21:35:42 -0500

    Cambio de codigo
    Nuevo codigo

--- js/app.js
+++ js/app.js
@@ -1,79 +1,70 @@
 //Objeto Calculadora
-//Seleccion de teclas
-var teclas = document.getElementsByClassName('tecla');
-for(i = 0; i < teclas.length; i++){
-  teclas[i].addEventListener('click', function(){
-      var display = document.getElementById('display').innerHTML;
-      var tecla = this.id;
-      switch(tecla){
-            //Borra la pantalla y la deja en 0
-        case 'on':
-          document.getElementById('display').innerHTML=0
-          break;
-            //Validar y borrar el 0 inicial
-        function qcero(num){
-          if (display==0) {
-              document.getElementById('display').innerHTML=num
-            }
-          else {
-              document.getElementById("display").innerHTML+=num
-            }};
-            //Ingreso de numeros
-        case '1':
-          qcero(1);
-          break;
-        case '2':
-          qcero(2);
-            break;
-        case '3':
-          qcero(3);
-            break;
-        case '4':
-          qcero(4);
-            break;
-        case '5':
-          qcero(5);
-            break;
-        case '6':
-          qcero(6);
-            break;
-        case '7':
-          qcero(7);
-            break;
-        case '8':
-          qcero(8);
-            break;
-        case '9':
-          qcero(9);
-            break;
-        case '0':
-          qcero(0);
-            break;
-        case 'punto':
-          function punto(){
-            if (display.indexOf(".") == -1) {
-                document.getElementById('display').innerHTML+= ".";
-              }}
-              punto();
-            break;
-        case 'sign':
-          function signo(){
-            document.getElementById("display").innerHTML*= -1;
-              }
-              signo();
-            break;
-      }
-    });
+var calc = {
+	display: document.getElementById("display").innerHTML,
 
-//Operaciones
 
+	init: (function(){
+		this.animacion();
+		this.teclado();
+	}),
 //Animacion de teclas
-  teclas[i].addEventListener('mousedown', function(){
-    this.style.padding = '2px';
-  });
+  animacion: function(){
+    var teclas = document.getElementsByClassName('tecla');
+      for(i = 0; i < teclas.length; i++){
+        teclas[i].addEventListener('mousedown', function(){
+          this.style.padding = '2px';
+          });
+        teclas[i].addEventListener('mouseup', function(){
+          this.style.padding = '0px';
+          });
+  }},
+//Asignacion de teclas
+  teclado: function(){
+  	document.getElementById("0").addEventListener("click", function() {calc.numeros("0");});
+  	document.getElementById("1").addEventListener("click", function() {calc.numeros("1");});
+  	document.getElementById("2").addEventListener("click", function() {calc.numeros("2");});
+  	document.getElementById("3").addEventListener("click", function() {calc.numeros("3");});
+  	document.getElementById("4").addEventListener("click", function() {calc.numeros("4");});
+  	document.getElementById("5").addEventListener("click", function() {calc.numeros("5");});
+  	document.getElementById("6").addEventListener("click", function() {calc.numeros("6");});
+  	document.getElementById("7").addEventListener("click", function() {calc.numeros("7");});
+  	document.getElementById("8").addEventListener("click", function() {calc.numeros("8");});
+  	document.getElementById("9").addEventListener("click", function() {calc.numeros("9");});
+  	document.getElementById("on").addEventListener("click", function() {calc.on();});
+  	document.getElementById("sign").addEventListener("click", function() {calc.signo();});
+  	document.getElementById("punto").addEventListener("click", function() {calc.punto();});
+  	document.getElementById("igual").addEventListener("click", function() {calc.igual();});
+  	document.getElementById("dividido").addEventListener("click", function() {calc.operaciones("/");});
+  	document.getElementById("por").addEventListener("click", function() {calc.operaciones("*");});
+  	document.getElementById("menos").addEventListener("click", function() {calc.operaciones("-");});
+  	document.getElementById("mas").addEventListener("click", function() {calc.operaciones("+");});
+  },
+//Funciones de las teclas
+	numeros: function(num){
+    if (this.display.length<8) {
+      if (this.display==0) {
+          document.getElementById('display').innerHTML=num
+        }
+      else {
+          document.getElementById("display").innerHTML+=num
+        }}
+	},
+  on: function(){
+    document.getElementById('display').innerHTML=0;
+  },
+  signo: function(){
+    document.getElementById('display').innerHTML*=-1;
+  },
+  punto: function(){
 
-  teclas[i].addEventListener('mouseup', function(){
-    this.style.padding = '0px';
-  });
+  },
+  igual: function(){
 
-}
+  },
+  operaciones: function(){
+
+  },
+
+//Fin de Calculadora
+};
+calc.init();
