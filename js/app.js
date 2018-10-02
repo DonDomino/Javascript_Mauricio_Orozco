5e168824fd425c2ec441c0d25302ca9f7106ba1c
Mauricio <maudob@live.com>
2018-10-01 19:38:10 -0500

    Final

--- js/app.js
+++ js/app.js
@@ -1,82 +1,154 @@
-//Funciones de teclas
-var teclas = document.getElementsByClassName('tecla');
-for(i = 0; i < teclas.length; i++){
-//Ingreso de numeros
-  teclas[i].addEventListener('click', function(){
-			var display = document.getElementById('display').innerHTML;
-      var tecla = this.id;
-      switch(tecla){
-//Reinicia el display a 0
-        case 'on':
-          document.getElementById('display').innerHTML=0
-          break;
-//Funciones
-//Valida 8 caracteres en display y no repeticion del 0 inicial
-					function validar(num){
-						if (display.length < 8) {
-							if (display==0) {
-								document.getElementById('display').innerHTML=num
-								}
-						else {
-								document.getElementById("display").innerHTML+=num
-							}}};
-//Coloca el punto y valida no repetirlo
-					function punto(){
-            if (display.indexOf(".") == -1) {
-                document.getElementById('display').innerHTML+= ".";
-              }};
+//Objeto calculadora
+var calc = {
+	display: document.getElementById("display"),
+	valorDisplay: '0',
+	operacion: '0',
+	operando1: '0',
+	operando2: '0',
+	cache: '0',
+	cacheIgual: false,
+	resultado: '0',
+//funcion que inicia la calculadora
+  init: (function(){
+		this.animacion();
+		this.teclado();
+	}),
+//Animacion de botones
+  animacion: function(){
+    var teclas = document.getElementsByClassName('tecla');
+      for(i = 0; i < teclas.length; i++){
+        teclas[i].addEventListener('mousedown', function(){
+          this.style.padding = '2px';
+          });
+        teclas[i].addEventListener('mouseup', function(){
+          this.style.padding = '0px';
+          });
+  }},
+//Asignacion de teclas
+  teclado: function(){
+  	document.getElementById("0").addEventListener("click", function() {calc.numeros("0");});
+  	document.getElementById("1").addEventListener("click", function() {calc.numeros("1");});
+  	document.getElementById("2").addEventListener("click", function() {calc.numeros("2");});
+  	document.getElementById("3").addEventListener("click", function() {calc.numeros("3");});
+  	document.getElementById("4").addEventListener("click", function() {calc.numeros("4");});
+  	document.getElementById("5").addEventListener("click", function() {calc.numeros("5");});
+  	document.getElementById("6").addEventListener("click", function() {calc.numeros("6");});
+  	document.getElementById("7").addEventListener("click", function() {calc.numeros("7");});
+  	document.getElementById("8").addEventListener("click", function() {calc.numeros("8");});
+  	document.getElementById("9").addEventListener("click", function() {calc.numeros("9");});
+  	document.getElementById("on").addEventListener("click", function() {calc.on();});
+  	document.getElementById("sign").addEventListener("click", function() {calc.signo();});
+  	document.getElementById("punto").addEventListener("click", function() {calc.punto();});
+  	document.getElementById("igual").addEventListener("click", function() {calc.igual();});
+  	document.getElementById("dividido").addEventListener("click", function() {calc.operaciones("/");});
+  	document.getElementById("por").addEventListener("click", function() {calc.operaciones("*");});
+  	document.getElementById("menos").addEventListener("click", function() {calc.operaciones("-");});
+  	document.getElementById("mas").addEventListener("click", function() {calc.operaciones("+");});
+  },
+//Funciones de las teclas
+//Actualiza el display segun las funciones
+	newDisplay: function () {
+		this.display.innerHTML = this.valorDisplay;
+},
+//Ingreso de numeros y validacion de no repetir 0 inicial ni sobrepasar 8 caracteres
+	numeros: function(num){
+    if (this.valorDisplay.length < 8) {
+      if (this.valorDisplay == "0") {
+					this.valorDisplay ="";
+          this.valorDisplay = this.valorDisplay + num;
+        }
+      else {
+          this.valorDisplay = this.valorDisplay + num;
+        }
+				this.newDisplay();
+			}
+	},
+//Reinicia la calculadora
+  on: function(){
+    this.valorDisplay = '0';
+		this.operacion = "",
+		this.operando1 = 0,
+		this.operando2 = 0,
+		this.cache = 0,
+		this.cacheIgual = false,
+		this.resultado = 0,
+		this.newDisplay();
+  },
 //Coloca el negativo
-					function signo(){
-						document.getElementById("display").innerHTML*= -1;
-							}
-        case '1':
-				validar(1);
-          break;
-        case '2':
-				validar(2);
-            break;
-        case '3':
-				validar(3);
-            break;
-        case '4':
-				validar(4);
-            break;
-        case '5':
-				validar(5);
-            break;
-        case '6':
-				validar(6);
-            break;
-        case '7':
-				validar(7);
-            break;
-        case '8':
-				validar(8);
-            break;
-        case '9':
-				validar(9);
-            break;
-        case '0':
-				validar(0);
-            break;
-        case 'punto':
-				punto();
-            break;
-				case 'sign':
-				signo()
-						break;
-      }
-    });
-
-//Operaciones
-
-//Animacion de teclas
-  teclas[i].addEventListener('mousedown', function(){
-    this.style.padding = '2px';
-  });
-
-  teclas[i].addEventListener('mouseup', function(){
-    this.style.padding = '0px';
-  });
-
-}
+  signo: function(){
+		if (this.valorDisplay !="0") {
+			var temp;
+			if (this.valorDisplay.charAt(0)=="-") {
+				temp = this.valorDisplay.slice(1);
+			}	else {
+				temp = "-" + this.valorDisplay;
+			}
+		this.valorDisplay = "";
+		this.valorDisplay = temp;
+		this.newDisplay();
+		}
+  },
+//Coloca el decimal y valida que no se repita el punto
+  punto: function(){
+		if (this.valorDisplay.indexOf(".")== -1) {
+			if (this.valorDisplay == ""){
+				this.valorDisplay = this.valorDisplay + "0.";
+			} else {
+				this.valorDisplay = this.valorDisplay + ".";
+			}
+			this.newDisplay();
+		}
+  },
+//Ejecuta las operaciones
+  igual: function(){
+		if(!this.cacheIgual){
+			this.operando2 = parseFloat(this.valorDisplay);
+			this.cache = this.operando2;
+			this.calculos(this.operando1, this.operando2, this.operacion);
+		} else {
+//Presionar igual consecutivamente
+		this.calculos(this.operando1, this.cache, this.operacion);
+		}
+//Guarda el resultado como operando1 para continuar
+		this.operando1 = this.resultado;
+//Actualiza el display
+		this.valorDisplay = "";
+//valida la longitud de 8 caracteres
+		if (this.resultado.toString().length < 9){
+			this.valorDisplay = this.resultado.toString();
+		} else {
+			this.valorDisplay = this.resultado.toExponential(5);
+		}
+//Calcular sobre ultimo valor
+		this.cacheIgual = true;
+		this.newDisplay();
+  },
+//Toma el numero en pantalla como operando1
+  operaciones: function(operador){
+		this.operando1 = parseFloat(this.valorDisplay);
+				this.valorDisplay = "";
+				this.operacion = operador;
+				this.cacheIgual = false;
+				this.newDisplay();
+  },
+//Toma los operandos y ejecuta la operacion segun el parametro
+	calculos: function (operando1, operando2, operacion) {
+		switch(operacion){
+			case "+":
+				this.resultado = (operando1 + operando2);
+			break;
+			case "-":
+				this.resultado = (operando1 - operando2);
+			break;
+			case "*":
+				this.resultado = (operando1 * operando2);
+			break;
+			case "/":
+				this.resultado = (operando1 / operando2);
+			break;
+		}
+	},
+//Fin de Objeto Calculadora
+  };
+//Funcion que inicializa todo el codigo.
+  calc.init();
